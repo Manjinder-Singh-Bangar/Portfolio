e8c7de60eae98f719a92e93b38a2a1f815236c9f
Manjinder-Singh-Bangar <manusharpshooter@gmail.com>
2025-03-16 13:44:11 -0400

    handle not found blogs and also job title added in the blogs

--- Frontend/src/pages/DetailedBlog.jsx
+++ Frontend/src/pages/DetailedBlog.jsx
@@ -1,11 +1,16 @@
-import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useEffect, useRef, useState } from 'react'
+import { useParams, Link } from 'react-router-dom'
 import { blogs } from '../../public/assets/Data/blogs.js';
 import CurrentBlog from '../Components/CurrentBlog';
+import gsap from 'gsap';
 
+import { useGSAP } from '@gsap/react';
+
+gsap.registerPlugin(useGSAP)
 const DetailedBlog = () => {
     const { id } = useParams();
     const [currentBlog, setCurrentBlog] = useState(null);
+    const blogNotFoundRef = useRef()
 
     useEffect(() => {
         
@@ -14,9 +19,25 @@ const DetailedBlog = () => {
             setCurrentBlog(blog);
         }
     }, [id]);
+
+    useGSAP(() => {
+        gsap.to(blogNotFoundRef.current, {
+            opacity: 1,
+            duration: 0.6,
+            ease:"expo.inOut"
+        })
+    }, [blogNotFoundRef.current])
     
     if (!currentBlog) {
-        return <div>Blog not found</div>;
+        return (
+        <div ref={blogNotFoundRef} className='min-h-[calc(100vh-104.2px)] flex opacity-0 justify-center items-center w-full max-h-fit text-[#607B96]'>
+            <div className='flex flex-col h-full'>
+                <Link className='border-b-[1px] justify-self-start w-fit pb-1' to={"/blogs"}>Go back to blogs</Link>
+                <h1 className='text-[50px]'>Blog not found</h1>
+
+            </div>
+        </div>
+        )
     }
 
     return (
@@ -27,6 +48,7 @@ const DetailedBlog = () => {
                 currentReadMeFile={currentBlog.fileName}
                 currentBlogAuthorLocation={currentBlog.location}
                 currentBlogDateCreated={currentBlog.dayWritten}
+                currentBlogAuthorJob={currentBlog.job}
             />
         </section>
     );
