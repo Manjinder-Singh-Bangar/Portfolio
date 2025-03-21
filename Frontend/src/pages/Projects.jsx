297cceffa2b95ab90bf2d852a01e402bef8576a1
Manjinder-Singh-Bangar <manusharpshooter@gmail.com>
2025-03-21 11:50:34 -0400

    Projects page responsive

--- Frontend/src/pages/Projects.jsx
+++ Frontend/src/pages/Projects.jsx
@@ -20,23 +20,23 @@ const Projects = () => {
   
   
   return (
-  <div className='w-full flex lg:h-[calc(100vh-104.2px)] overflow-hidden text-[#607B96] items-center h-screen'>
-    <div className='h-full border-r-[1px] overflow-y-scroll flex-[0.3] border-[#1E2D3D] sticky top-0'>
+  <div className='w-full flex xs:flex-col lg:flex-row lg:h-[calc(100vh-104.2px)] xs:overflow-x-hidden lg:overflow-y-hidden text-[#607B96] items-center xs:h-fit'>
+    <div className='h-full border-r-[1px] xs:overflow-y-hidden lg:overflow-y-scroll xs:flex-[1] lg:flex-[0.34] border-[#1E2D3D] sticky top-0'>
       <div className={` ${isSkillsOpened ? "pb-6 h-fit gap-3  border-b-[1px]" : "h-[51.8px] gap-0 border-b-0 p-0"} transition-all flex-col flex border-[#1E2D3D]`}>
-        <button onClick={() => setIsSkillsOpened((prev) => !prev)} className={`flex transition-all bg-[#011627] gap-3 z-10 text-[18px] ${isSkillsOpened ? "text-white" : ""} w-full px-6 py-3 border-b-[1px] border-[#1E2D3D]`}><img className={`${isSkillsOpened ? "rotate-0" : "rotate-[-90deg]"}`} src={arrowFilledSvgSrc} alt="" />skills</button>
+        <button onClick={() => setIsSkillsOpened((prev) => !prev)} className={`flex transition-all bg-[#011627] gap-3 z-10 text-[18px] ${isSkillsOpened ? "text-white" : ""} w-full px-6 py-3 border-b-[1px] items-center border-[#1E2D3D]`}><img className={`${isSkillsOpened ? "rotate-0" : "rotate-[-90deg]"} w-[12px] h-[12px]`} src={arrowFilledSvgSrc} alt="" />skills</button>
         <div className={`grid grid-cols-3 w-full gap-6 p-6 transition-all text-[16px] relative ${isSkillsOpened ? "top-[0]  opacity-1 h-fit" : "p-0 opacity-0 z-[-1] top-[-100px] h-0"}`}>
             {
               skills.map((item) =>{
                 return (
-                  <img className={`w-[30px] hover:scale-[1.3] transition-all scale-[1] h-[30px] ${isSkillsOpened ? "relative" : "absolute"}`} src={item} />
+                  <img className={`w-[30px] mx-auto hover:scale-[1.3] transition-all scale-[1] h-[30px] ${isSkillsOpened ? "relative" : "absolute"}`} src={item} />
                 )
               })
              }
         </div>
       </div>
       <div className={` ${isExperienceOpened ? "pb-6 h-fit gap-3  " : "h-fit gap-0 border-b-0 p-0"} transition-all flex-col h-fit flex border-[#1E2D3D]`}>
-          <button onClick={() => setIsExperienceOpened((prev) => !prev)} className={`flex transition-all bg-[#011627] gap-3 z-10 text-[18px] ${isExperienceOpened ? "text-white" : ""} w-full px-6 py-3 border-b-[1px] border-[#1E2D3D]`}><img className={`${isExperienceOpened ? "rotate-0" : "rotate-[-90deg]"}`} src={arrowFilledSvgSrc} alt="" />experience</button>
-          <div className={`grid w-full gap-6 p-6 transition-all text-[16px] relative ${isExperienceOpened ? "top-[0]  opacity-1 h-fit" : "p-0 opacity-0 z-[-1] top-[-100px] h-0"}`}>
+          <button onClick={() => setIsExperienceOpened((prev) => !prev)} className={`flex transition-all bg-[#011627] gap-3 z-10 text-[18px] ${isExperienceOpened ? "text-white" : ""} w-full px-6 py-3 border-b-[1px] items-center border-[#1E2D3D]`}><img className={`${isExperienceOpened ? "rotate-0" : "rotate-[-90deg]"} w-[12px] h-[12px]`} src={arrowFilledSvgSrc} alt="" />experience</button>
+          <div className={`grid w-full md:grid-cols-3 xs:grid-cols-2 lg:grid-cols-1 gap-6 p-6 transition-all text-[16px] relative ${isExperienceOpened ? "top-[0]  opacity-1 h-fit" : "p-0 opacity-0 z-[-1] top-[-100px] h-0"}`}>
               {experience.map((item) => {
                 return (
                   <Experience picture={item.picture} startingTime={item.startingTime} endingTime={item.endingTime} projectName={item.pr}/>
@@ -45,7 +45,7 @@ const Projects = () => {
           </div>
       </div>
     </div>
-    <div className='p-7 flex-[1.7] gap-4 flex'>
+    <div className='p-7 lg:flex-[1.5] grid lg:grid-cols-3 xs:grid-cols-1 md:grid-cols-2 md:flex-[1.66] gap-4 '>
       {
         projects.map((item) =>{
           return (
