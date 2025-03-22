4fc743ec84a7eb573dda22e05cd97cd6df58b1b6
Manjinder-Singh-Bangar <manusharpshooter@gmail.com>
2025-03-22 17:53:48 -0400

    blog page is responsive

--- Frontend/src/Components/BlogCard.jsx
+++ Frontend/src/Components/BlogCard.jsx
@@ -7,25 +7,25 @@ const BlogCard = ({blogThumbnailImageSrc, blogHeading, blogAuthorJob, blogWritte
   return (
     
         <div key={blogKey} id='blogCard' className={`text-[#607B96] border-y-[1px] border-[#1E2D3D] p-8 gap-6 grid grid-cols-4 w-4/6 ${blogClassName}`}>
-            <div className='col-span-1 flex items-center'>
+            <div id='imageContainer' className='col-span-1 flex items-center'>
                 <img className='w-full' src={blogThumbnailImageSrc} alt="" />
             </div>
             <div className='col-span-3 justify-center flex flex-col gap-4'>
                 <h1 className='lg:text-2xl xs:text-xl'>{blogHeading}</h1>
                 <div className='flex justify-between'>
-                <div className='flex gap-3 items-center'>
-                    <img className='w-[40px] my-auto h-[40px] rounded-full' src={blogAuthorProfilePicture} alt="" />
-                    <div className='flex flex-col'>
-                    <p>{blogAuthor}</p>
-                    <p>{blogAuthorJob}</p>
+                    <div className='flex gap-3 items-center'>
+                        <img className='w-[40px] my-auto h-[40px] rounded-full' src={blogAuthorProfilePicture} alt="" />
+                        <div className='flex flex-col'>
+                        <p>{blogAuthor}</p>
+                        <p>{blogAuthorJob}</p>
+                        </div>
                     </div>
-                </div>
-                
-                <div className='flex flex-col gap-1'>
-                <p>{blogWrittenDate}</p>
-                <Link to={`/blogs/${blogId}`} className='bg-[#FEA55F] text-center p-2 text-white rounded-md'>Read Me</Link>
+                    
+                    <div className='flex flex-col gap-1'>
+                    <p>{blogWrittenDate}</p>
+                    <Link to={`/blogs/${blogId}`} className='bg-[#FEA55F] text-center p-2 text-white rounded-md'>Read Me</Link>
 
-                </div>
+                    </div>
 
                 </div>
             </div>
